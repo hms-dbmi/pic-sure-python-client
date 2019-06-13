8060e974fdbc02d736b474e2658f55f4612ac10a
Nick Benik <nbenik@gmail.com>
2019-06-13 12:48:47 -0400

    Add option to have connector accept a self-signed certificate, fixed minor bug that was discovered during change

--- PicSureClient/Connection.py
+++ PicSureClient/Connection.py
@@ -15,17 +15,17 @@ class Client:
     def help(self):
         print("""
         [HELP] PicSureClient.Client()
-            .version()                      gives version information for library
-            .connect(<url>, <token>)        returns a connection object
+            .version()                                                 gives version information for library
+            .connect(<url>, <token> [, allowSelfSignedSSL = True])     returns a connection object
         """)
 
     @classmethod
-    def connect(self, url, token):
+    def connect(self, url, token, allowSelfSignedSSL = False):
         """ PicSure.connect returns a configured instance of a PicSureClient.Connection class """
-        return PicSureClient.Connection(url, token)
+        return PicSureClient.Connection(url, token, allowSelfSignedSSL)
 
 class Connection:
-    def __init__(self, url, token):
+    def __init__(self, url, token, allowSelfSignedSSL = False):
         tempurl = url.strip()
         if tempurl.endswith("/"):
             tempurl = url
@@ -33,10 +33,10 @@ class Connection:
             tempurl = tempurl + "/"
         self.url = tempurl
         self._token = token
-
+        self.AllowSelfSigned = allowSelfSignedSSL
     def help(self):
         print("""
-        [HELP] PicSureClient.Client.connect(url, token)
+        [HELP] PicSureClient.Client.connect(url, token [, allowSelfSignedSSL = True])
             .list()                         Prints a list of available resources
             .about(resource_uuid)           Prints details about a specific resource
             
@@ -54,6 +54,10 @@ class Connection:
               a specific type which has its own adapter library.  Libraries will
               follow the naming convention: "PicSureXyzLib" where "Xyz" 
               specifies the adapter's storage format.
+              
+            ** By default, self-signed SSL connections are rejected.  To change
+              this behavior pass "allowSelfSignedSSL=True" when creating a 
+              PIC-SURE Connection instance.
         """)
 
     def about(self, resourceId = None):
@@ -65,19 +69,19 @@ class Connection:
         else:
             url = url + str(resourceId)
 
-        httpConn = httplib2.Http()
+        httpConn = httplib2.Http(disable_ssl_certificate_validation=self.AllowSelfSigned)
         httpHeaders = {'Content-Type':'application/json', 'Authorization':'Bearer '+self._token}
         (resp_headers, content) = httpConn.request(url, "GET", headers=httpHeaders)
         if resp_headers["status"] != "200":
             print("ERROR: HTTP response was bad")
             print(resp_headers)
             print(content.decode("utf-8"))
-            return {"error":True, "headers":resp_headers, "content":content.decode("utf-8")}
+            return {"error":True, "headers":resp_headers, "content":json.loads(content.decode("utf-8"))}
         else:
             return {"error":False, "headers":resp_headers, "content":json.loads(content.decode('utf-8'))}
 
     def list(self):
-        listing = self.getResources()
+        listing = json.loads(self.getResources())
         print("+".ljust(39, '-') + '+'.ljust(55, '-'))
         print("|  Resource UUID".ljust(39, ' ') + '|  Resource Name'.ljust(50, ' '))
         print("+".ljust(39, '-') + '+'.ljust(55, '-'))
@@ -91,7 +95,7 @@ class Connection:
 
     def getResources(self):
         """PicSureClient.resources() function is used to list all resources on the connected endpoint"""
-        httpConn = httplib2.Http()
+        httpConn = httplib2.Http(disable_ssl_certificate_validation=self.AllowSelfSigned)
         httpHeaders = {'Content-Type':'application/json', 'Authorization':'Bearer '+self._token}
         url = self.url + "info/resources"
         (resp_headers, content) = httpConn.request(url, "GET", headers=httpHeaders)
@@ -110,12 +114,17 @@ class Connection:
 
 class PicSureConnectionAPI:
     def __init__(self, url, token):
+        # make sure passed URL ends in slash
+        url = url.strip()
+        if not url.endswith("/"):
+            url = url + "/"
+        # save values
         self.url = url
         self._token = token
 
     def info(self, resource_uuid):
         # https://github.com/hms-dbmi/pic-sure/blob/master/pic-sure-resources/pic-sure-resource-api/src/main/java/edu/harvard/dbmi/avillach/service/ResourceWebClient.java#L43
-        httpConn = httplib2.Http()
+        httpConn = httplib2.Http(disable_ssl_certificate_validation=self.AllowSelfSigned)
         httpHeaders = {'Content-Type':'application/json', 'Authorization':'Bearer '+self._token}
         url = self.url + "info/" + resource_uuid
         (resp_headers, content) = httpConn.request(url, "POST", headers=httpHeaders, body="{}")
@@ -126,12 +135,12 @@ class PicSureConnectionAPI:
             print(content.decode("utf-8"))
             return '{"results":{}, "error":"true"}'
         else:
-            return json.loads(content.decode("utf-8"))
+            return content.decode("utf-8")
 
     def search(self, resource_uuid, query):
         # make sure a Resource UUID is passed via the body of these commands
         # https://github.com/hms-dbmi/pic-sure/blob/master/pic-sure-resources/pic-sure-resource-api/src/main/java/edu/harvard/dbmi/avillach/service/ResourceWebClient.java#L69
-        httpConn = httplib2.Http()
+        httpConn = httplib2.Http(disable_ssl_certificate_validation=self.AllowSelfSigned)
         httpHeaders = {'Content-Type':'application/json', 'Authorization':'Bearer '+self._token}
         if query == None:
             bodystr = json.dumps({"query":""})
@@ -157,7 +166,7 @@ class PicSureConnectionAPI:
     def syncQuery(self, resource_uuid, query):
         # make sure a Resource UUID is passed via the body of these commands
         # https://github.com/hms-dbmi/pic-sure/blob/master/pic-sure-resources/pic-sure-resource-api/src/main/java/edu/harvard/dbmi/avillach/service/ResourceWebClient.java#L186
-        httpConn = httplib2.Http()
+        httpConn = httplib2.Http(disable_ssl_certificate_validation=self.AllowSelfSigned)
         httpHeaders = {'Content-Type':'application/json', 'Authorization':'Bearer '+self._token}
         url = self.url + "query/sync"
         (resp_headers, content) = httpConn.request(url, "POST", headers=httpHeaders, body=query)
